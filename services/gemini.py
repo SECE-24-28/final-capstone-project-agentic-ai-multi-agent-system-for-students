766abed5a84986bf04111a51fe46a03e8fedaf7f
KEERTHI AANAND K S <keerthiaanand77@gmail.com>
2026-06-11 19:39:47 +0530

    Update model version in generate function to use llama-3.1-8b-instant

--- services/gemini.py
+++ services/gemini.py
@@ -18,7 +18,7 @@ def generate(prompt):
         response = client.chat.completions.create(
 
             model=
-            "llama-3.3-70b-versatile",
+            "llama-3.1-8b-instant",
 
             messages=[
                 {
